1ec0aca4bd3958b74e286e40896228ad1fd2b72f
Anna Kudas <nnkudas@gmail.com>
2023-08-25 10:25:10 +0200

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+console.log(galleryItems);
+const imageList = document.querySelector('.gallery');
+imageList.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+imageList.addEventListener('click', handlerClick);
+
+function createMarkup(arr) {
+    return arr.map(({ preview, original, description }) =>
+        `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img 
+                    class="gallery__image"
+                    src="${preview}" 
+                    data-source="${original}"
+                    width="340"
+                    alt="${description}" />
+            </a>
+        </li>`
+    ).join('');
+}
+
+function handlerClick(evt) {
+    evt.preventDefault();
+    const clickedElement = evt.target;
+
+    if (!clickedElement.classList.contains('gallery__image')) {
+        return;
+    }
+
+    const source = clickedElement.dataset.source;
+
+    // Use simpleLightbox's functions instead of basicLightbox
+    const gallery = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
+}
 
 console.log(galleryItems);
